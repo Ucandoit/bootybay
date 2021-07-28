27fbcfc732b40a39a1bc491a778fd716643c4969
Ucandoit <yukan.zhao@gmail.com>
2021-07-28 22:31:52 +0200

    feat(auction-history): add mongodb config

--- apps/auction-history/src/app.module.ts
+++ apps/auction-history/src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuctionsModule } from './auctions/auctions.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [AuctionsModule],
+  imports: [
+    AuctionsModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get<string>('database.host')}:${configService.get<number>('database.port')}`,
+        user: configService.get<string>('database.user'),
+        pass: configService.get<string>('database.password'),
+        dbName: configService.get<string>('database.name'),
+      }),
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
